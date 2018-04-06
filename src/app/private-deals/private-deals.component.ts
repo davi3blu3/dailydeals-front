1fd2b41e4d06fd6c8e815bd0242b655b8c295436
Dave Harned <dharned@gmail.com>
2018-04-06 00:18:39 -0500

    flesh out components

--- src/app/private-deals/private-deals.component.ts
+++ src/app/private-deals/private-deals.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { Deal } from '../deal';
+// We haven't defined these services yet
+import { AuthService } from '../auth.service';
+import { DealService } from '../deal.service';
 
 @Component({
   selector: 'app-private-deals',
   templateUrl: './private-deals.component.html',
   styleUrls: ['./private-deals.component.css']
 })
-export class PrivateDealsComponent implements OnInit {
+export class PrivateDealsComponent implements OnInit, OnDestroy {
+  dealsSub: Subscription;
+  privateDeals: Deal[];
+  error: any;
 
-  constructor() { }
+  constructor(
+    public dealService: DealService,
+    public authService: AuthService
+  ) { }
 
   ngOnInit() {
+    this.dealsSub = this.dealService
+      .getPrivateDeals()
+      .subscribe(
+        deals => this.privateDeals = deals,
+        err => this.error = err
+    );
+  }
+
+  ngOnDestroy() {
+    this.dealsSub.unsubscribe();
   }
 
 }
